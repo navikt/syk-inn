812d19054943def31fa2b9060f743f040ccbcceb
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2024-10-22 09:58:07 +0200

    edit client-id and set basic minimum scopes

--- src/app/api/fhir-mock/auth/[[...path]]/route.ts
+++ src/app/api/fhir-mock/auth/[[...path]]/route.ts
@@ -36,7 +36,7 @@ async function handler(req: Request): Promise<Response> {
                 id_token: await createIdToken(),
                 token_type: 'Bearer',
                 expires_in: 3600,
-                scope: 'patient/*.read launch',
+                scope: 'openid profile launch fhirUser patient/*.* user/*.* offline_access',
                 need_patient_banner: true,
                 smart_style_url: `${getAbsoluteURL()}/api/fhir/smart-style.json`,
                 patient: 'cd09f5d4-55f7-4a24-a25d-a5b65c7a8805',
