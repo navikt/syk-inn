07a4352bad0ed76ae7f5df2db9c46f1ea36e7dff
Karl J. Overå <k@rl.run>
2024-10-23 10:36:35 +0200

    chore: run tests and lint on GHA

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     output: 'standalone',
     assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    eslint: { ignoreDuringBuilds: true },
     experimental: {
         optimizePackageImports: ['@navikt/ds-react', '@navikt/aksel-icons'],
         instrumentationHook: true,
