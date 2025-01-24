9f8da8d9508ecf7ccfd2e15a40c00365438f46fe
Karl J. Overå <k@rl.run>
2025-01-24 11:42:37 +0100

    chore: remove /gib-token

--- src/app/(standalone)/gib-token/route.tsx
+++
@@ -1,5 +0,0 @@
-export function GET(request: Request): Response {
-    const bearer = request.headers.get('Authorization')
-
-    return new Response(bearer?.replace('Bearer ', '') ?? 'nothing to see here')
-}
