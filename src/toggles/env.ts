1c2ddf1738f74f06899087ca544cf064ab4fbd4b
Karl J. Overå <k@rl.run>
2025-02-21 14:50:12 +0100

    chore: explicit dev toggles [skip ci]

--- src/toggles/env.ts
+++ src/toggles/env.ts
@@ -1,21 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { IToggle } from '@unleash/nextjs'
 
 import { bundledEnv } from '@utils/env'
 
-import { EXPECTED_TOGGLES } from './toggles'
+import { ExpectedToggles } from './toggles'
+
+const on: Omit<IToggle, 'name'> = {
+    enabled: false,
+    impressionData: false,
+    variant: { enabled: true, name: 'default' },
+}
+
+const off: Omit<IToggle, 'name'> = {
+    enabled: false,
+    impressionData: false,
+    variant: { enabled: false, name: 'default' },
+}
+
+const devToggles: Record<ExpectedToggles, IToggle> = {
+    SYK_INN_SECURE_AUTH: {
+        name: 'SYK_INN_SECURE_AUTH',
+        ...off,
+    },
+    SYK_INN_DEBUG_WAIT_BEFORE_LAUNCH: {
+        name: 'SYK_INN_DEBUG_WAIT_BEFORE_LAUNCH',
+        ...off,
+    },
+    SYK_INN_TIDLIGERE_SYKMELDINGER: {
+        name: 'SYK_INN_TIDLIGERE_SYKMELDINGER',
+        ...on,
+    },
+}
 
 export function localDevelopmentToggles(): IToggle[] {
-    return EXPECTED_TOGGLES.map(
-        (it): IToggle => ({
-            name: it,
-            enabled: false,
-            impressionData: false,
-            variant: {
-                enabled: true,
-                name: 'default',
-            },
-        }),
-    )
+    return Object.values(devToggles)
 }
 
 export function getUnleashEnvironment(): 'development' | 'production' {
