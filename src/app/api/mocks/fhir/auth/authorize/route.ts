d84fa4f714c5dad5fd2a8172fb0e739b14e7f045
Karl J. Overå <k@rl.run>
2025-02-25 16:19:19 +0100

    chore: move auth mocks to distinct routes

---
+++ src/app/api/mocks/fhir/auth/authorize/route.ts
@@ -0,0 +1,34 @@
+import { randomUUID } from 'node:crypto'
+
+import { logger as pinoLogger } from '@navikt/next-logger'
+import { notFound } from 'next/navigation'
+
+import { isE2E, isLocalOrDemo } from '@utils/env'
+
+const logger = pinoLogger.child({}, { msgPrefix: '[FHIR-MOCK-Auth] ' })
+
+export function GET(request: Request): Response {
+    if (!isLocalOrDemo && !isE2E) {
+        notFound()
+    }
+
+    const url = new URL(request.url)
+
+    logger.info(`/auth/authorize request with params: \n\t${Array.from(url.searchParams.entries()).join('\n\t')}`)
+
+    const redirectUri = url.searchParams.get('redirect_uri')
+    if (!redirectUri) {
+        return new Response('Missing redirect_uri', { status: 400 })
+    }
+
+    const state = url.searchParams.get('state')
+    if (!state) {
+        return new Response('Missing state', { status: 400 })
+    }
+
+    const notAToken = randomUUID()
+    const redirectUrl = `${redirectUri}?code=${notAToken}&state=${state}`
+
+    logger.info(`/auth/authorize good, redirecting to ${redirectUrl}`)
+    return Response.redirect(redirectUrl, 302)
+}
