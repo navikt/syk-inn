02a9fe2da42dc8d1be83e5ec29543b4a4937da0e
Karl J. Overå <k@rl.run>
2025-05-12 12:16:44 +0200

    chore: remove unused local devtools feature [skip ci]

--- src/devtools/InternalDevTools.tsx
+++ src/devtools/InternalDevTools.tsx
@@ -1,9 +1,7 @@
 import React, { ReactElement, startTransition } from 'react'
-import { BodyShort, Button, Checkbox, CheckboxGroup, Detail, Heading } from '@navikt/ds-react'
+import { BodyShort, Button, Checkbox, CheckboxGroup, Heading } from '@navikt/ds-react'
 import { XMarkIcon } from '@navikt/aksel-icons'
 import { useQueryClient } from '@tanstack/react-query'
-
-import { getAbsoluteURL, pathWithBasePath } from '@utils/url'
 
 import { DataService } from '../data-fetcher/data-service'
 
@@ -32,7 +30,6 @@ export function InternalDevToolsPanel({ onClose }: Props): ReactElement {
             <div className="grid grid-cols-1 gap-6 mt-6">
                 <FeatureToggles />
                 <ToggleAPIFailures />
-                <ResetSmartContext />
             </div>
         </div>
     )
@@ -73,45 +70,6 @@ function FeatureToggles(): ReactElement {
                 >
                     Apply changed toggles
                 </Button>
-            </div>
-        </DevToolItem>
-    )
-}
-
-function ResetSmartContext(): ReactElement {
-    const resetSmartContext = (postAction?: 'reload' | 're-launch') => () => {
-        const key = sessionStorage.getItem('SMART_KEY')
-        if (key == null) return
-
-        sessionStorage.removeItem(key)
-        sessionStorage.removeItem('SMART_KEY')
-
-        if (postAction === 'reload') window.location.reload()
-        if (postAction === 're-launch') {
-            window.location.href = pathWithBasePath(`/fhir/launch?iss=${getAbsoluteURL()}/api/mocks/fhir`)
-        }
-    }
-
-    return (
-        <DevToolItem
-            title="SMART Context"
-            description="Utilities for interacting with SMART stuff"
-            className="flex flex-col gap-3"
-        >
-            <div>
-                <Detail>Deletes all SMART context from sessionStorage.</Detail>
-                <Detail spacing>Current SMART_KEY: {sessionStorage?.getItem('SMART_KEY') ?? 'none'}</Detail>
-                <div className="flex gap-3">
-                    <Button variant="secondary-neutral" size="small" onClick={resetSmartContext()}>
-                        Reset
-                    </Button>
-                    <Button variant="secondary-neutral" size="small" onClick={resetSmartContext('reload')}>
-                        Reset and reload
-                    </Button>
-                    <Button variant="secondary-neutral" size="small" onClick={resetSmartContext('re-launch')}>
-                        Reset and re-launch
-                    </Button>
-                </div>
             </div>
         </DevToolItem>
     )
