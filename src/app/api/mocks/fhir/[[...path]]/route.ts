d503d5d77e310a849185ef573428c2f47db745ce
Karl J. Overå <k@rl.run>
2025-06-02 22:11:59 +0200

    fix: demo fhir launch

--- src/app/api/mocks/fhir/[[...path]]/route.ts
+++ src/app/api/mocks/fhir/[[...path]]/route.ts
@@ -1,11 +1,9 @@
 import { getAbsoluteURL } from '@utils/url'
-import { bundledEnv } from '@utils/env'
 import { createFhirHandler } from '@navikt/fhir-mock-server/next'
 
 const handler = createFhirHandler({
     baseUrl: getAbsoluteURL(),
     fhirPath: '/api/mocks/fhir',
-    basePath: bundledEnv.NEXT_PUBLIC_BASE_PATH,
 })
 
 export {
