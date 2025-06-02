3bde098b0bbc253f6dc37a96b3ab86fef562442a
Karl J. Overå <k@rl.run>
2025-06-02 14:55:51 +0200

    fix: use correct OIDs for ICPC2 and ICD10

--- src/utils/oid.ts
+++ src/utils/oid.ts
@@ -1,8 +1,8 @@
 export type ICD10_OID = typeof ICD10_OID_VALUE
-export const ICD10_OID_VALUE = '2.16.578.1.12.4.1.1.7170'
+export const ICD10_OID_VALUE = '2.16.578.1.12.4.1.1.7110'
 
 export type ICPC2_OID = typeof ICPC2_OID_VALUE
-export const ICPC2_OID_VALUE = '2.16.578.1.12.4.1.1.7171'
+export const ICPC2_OID_VALUE = '2.16.578.1.12.4.1.1.7170'
 
 export type SupportedDiagnoseSystemsShorthand = 'ICD10' | 'ICPC2'
 
