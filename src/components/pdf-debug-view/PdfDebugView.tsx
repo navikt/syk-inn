9f2ceb829a5b5053844dcd611cf45528ff44f5cb
Karl J. Overå <k@rl.run>
2025-06-06 15:49:03 +0200

    fix: pdf debug view

--- src/components/pdf-debug-view/PdfDebugView.tsx
+++ src/components/pdf-debug-view/PdfDebugView.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement } from 'react'
-import { Heading } from '@navikt/ds-react'
+import { BodyShort, Detail, Heading } from '@navikt/ds-react'
+
+import { pathWithBasePath } from '@utils/url'
 
 type Props = {
     sykmeldingId: string
@@ -7,9 +9,16 @@ type Props = {
 
 function PdfDebugView({ sykmeldingId }: Props): ReactElement {
     return (
-        <div className="w-1/2 pb-8">
+        <div className="w-1/2 pb-16">
             <Heading size="small">PDF visning for sykmelding</Heading>
-            <object data={`/fhir/pdf/${sykmeldingId}`} type="application/pdf" width="100%" height="100%">
+            <Detail>Denne visningen er kun for demo, og skal fjernes.</Detail>
+            <BodyShort>Dette er dokumentet som vil bli lastet opp til WebMed</BodyShort>
+            <object
+                data={pathWithBasePath(`/fhir/pdf/${sykmeldingId}`)}
+                type="application/pdf"
+                width="100%"
+                height="100%"
+            >
                 <p>PDF cannot be displayed.</p>
             </object>
         </div>
