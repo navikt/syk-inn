cbe092e9b3bdd359d5b013a2169dcfa88846d351
Karl J. Overå <k@rl.run>
2025-06-20 15:15:54 +0200

    chore: try named lib tracer

--- libs/smart-on-fhir/src/client/otel.ts
+++ libs/smart-on-fhir/src/client/otel.ts
@@ -1,15 +1,19 @@
 import { trace, context, Span } from '@opentelemetry/api'
 
+const LIB_NAME = '@navikt/smart-on-fhir'
+const LIB_VERSION = '0.0.1-alpha.0'
+const SPAN_PREFIX = 'SmartClient.'
+
 export async function spanAsync<Result>(name: string, fn: (span: Span) => Promise<Result>): Promise<Result> {
-    const tracer = trace.getTracerProvider().getTracer('')
-    const span = tracer.startSpan(`SmartClient.${name}`)
+    const tracer = trace.getTracer(LIB_NAME, LIB_VERSION)
+    const span = tracer.startSpan(`${SPAN_PREFIX}${name}`)
 
     return context.with(trace.setSpan(context.active(), span), async () => fn(span).finally(() => span.end()))
 }
 
 export function spanSync<Result>(name: string, fn: () => Result): Result {
-    const tracer = trace.getTracerProvider().getTracer('')
-    const span = tracer.startSpan(`SmartClient.${name}`)
+    const tracer = trace.getTracer(LIB_NAME, LIB_VERSION)
+    const span = tracer.startSpan(`${SPAN_PREFIX}${name}`)
 
     return context.with(trace.setSpan(context.active(), span), () => {
         try {
