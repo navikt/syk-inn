c20b70c20c9ffc577b8206e885f397b344e18743
Karl J. Overå <k@rl.run>
2025-06-20 16:24:12 +0200

    chore: span fetchTokenExchange and record errors

--- libs/smart-on-fhir/src/client/smart/launch/token.ts
+++ libs/smart-on-fhir/src/client/smart/launch/token.ts
@@ -1,5 +1,7 @@
 import { InitialSession } from '../../storage/schema'
 import { logger } from '../../logger'
+import { spanAsync } from '../../otel'
+import { getResponseError } from '../../utils'
 
 import { TokenResponseSchema, TokenResponse } from './token-schema'
 
@@ -12,55 +14,65 @@ export async function fetchTokenExchange(
     redirect_uri: string,
     session: InitialSession,
 ): Promise<TokenResponse | TokenExchangeErrors> {
-    /**
-     * PKCE STEP 5
-     * Send code and the code_verifier (created in step 1) to the authorization servers /oauth/token endpoint.
-     */
-    const response = await fetch(session.tokenEndpoint, {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams({
-            client_id: 'syk-inn',
-            grant_type: 'authorization_code',
-            code: code,
-            code_verifier: session.codeVerifier,
-            redirect_uri: redirect_uri,
-        }),
-    })
+    return spanAsync('token-exchange', async (span) => {
+        span.setAttribute('fhir-server', session.server)
 
-    /**
-     * PKCE STEP 6
-     * Authorization server verifies the code_challenge and code_verifier.
-     * Upon successful verification the authorization server issues id_token, access_token and (optional) refresh_token.
-     */
-    if (!response.ok) {
-        logger.error(`Token exchange failed, token_endpoint responed with ${response.status} ${response.statusText}`)
-        if (response.headers.get('Content-Type')?.includes('text/plain')) {
-            const text = await response.text()
-            logger.error(`Token exchange failed with text: ${text}`)
-        } else if (response.headers.get('Content-Type')?.includes('application/json')) {
-            const json = await response.json()
-            logger.error(`Token exchange failed with json: ${JSON.stringify(json)}`)
+        /**
+         * PKCE STEP 5
+         * Send code and the code_verifier (created in step 1) to the authorization servers /oauth/token endpoint.
+         */
+        const response = await fetch(session.tokenEndpoint, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({
+                client_id: 'syk-inn',
+                grant_type: 'authorization_code',
+                code: code,
+                code_verifier: session.codeVerifier,
+                redirect_uri: redirect_uri,
+            }),
+        })
+
+        /**
+         * PKCE STEP 6
+         * Authorization server verifies the code_challenge and code_verifier.
+         * Upon successful verification the authorization server issues id_token, access_token and (optional) refresh_token.
+         */
+        if (!response.ok) {
+            const responseError = await getResponseError(response)
+            logger.error(
+                `Token exchange failed, token_endpoint responed with ${response.status} ${response.statusText}, server says: ${responseError}`,
+            )
+
+            span.recordException(
+                new Error(
+                    `Token exchange failed, token_endpoint responed with ${response.status} ${response.statusText}`,
+                ),
+            )
+
+            return { error: 'TOKEN_EXCHANGE_FAILED' }
         }
 
-        return { error: 'TOKEN_EXCHANGE_FAILED' }
-    }
+        const result: unknown = await response.json()
+        const parsedTokenResponse = TokenResponseSchema.safeParse(result)
 
-    const result: unknown = await response.json()
-    const parsedTokenResponse = TokenResponseSchema.safeParse(result)
+        if (!parsedTokenResponse.success) {
+            const exception = new Error(
+                `Issuer/token_endpoint ${session.tokenEndpoint} responded with weird token response`,
+                {
+                    cause: parsedTokenResponse.error,
+                },
+            )
 
-    if (!parsedTokenResponse.success) {
-        logger.error(
-            new Error(`Issuer/token_endpoint ${session.tokenEndpoint} responded with weird token response`, {
-                cause: parsedTokenResponse.error,
-            }),
-        )
+            logger.error(exception)
+            span.recordException(exception)
 
-        return { error: 'TOKEN_EXCHANGE_INVALID_BODY' }
-    }
+            return { error: 'TOKEN_EXCHANGE_INVALID_BODY' }
+        }
 
-    return parsedTokenResponse.data
+        return parsedTokenResponse.data
+    })
 }
