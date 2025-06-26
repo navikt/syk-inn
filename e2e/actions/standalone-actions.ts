e5b036201f2c62bce73cc0e3ce60b8c6ab3cd935
Karl J. Overå <k@rl.run>
2025-06-26 10:13:01 +0200

    chore: delete standalone e2e tests

--- e2e/actions/standalone-actions.ts
+++
@@ -1,7 +0,0 @@
-import { expect, Page } from '@playwright/test'
-
-export async function launchStandalone(page: Page): Promise<void> {
-    await page.goto(`/ny`)
-
-    await expect(page.getByRole('heading', { name: 'Opprett ny sykmelding' })).toBeVisible()
-}
