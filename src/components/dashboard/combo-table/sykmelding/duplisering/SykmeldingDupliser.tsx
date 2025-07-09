1e9647bcb2d7e342b82f752607bfc5573ad815cb
Karl J. Overå <k@rl.run>
2025-07-09 09:11:19 +0200

    feat: add dupliser-knapp in dashboard view

---
+++ src/components/dashboard/combo-table/sykmelding/duplisering/SykmeldingDupliser.tsx
@@ -0,0 +1,30 @@
+import React, { ReactElement, useRef } from 'react'
+import { Tooltip } from '@navikt/ds-react'
+import { TabsAddIcon } from '@navikt/aksel-icons'
+
+import { SykmeldingFragment } from '@queries'
+import { SlowNextLinkButton } from '@components/misc/SlowNextLinkButton'
+
+import { useAppDispatch } from '../../../../../providers/redux/hooks'
+import { nySykmeldingMultistepActions } from '../../../../../providers/redux/reducers/ny-sykmelding-multistep'
+
+import { dupliserSykmelding } from './duplisering-mapper'
+
+export function DupliserSykmeldingButton({ sykmelding }: { sykmelding: SykmeldingFragment }): ReactElement {
+    const nextDraftId = useRef(crypto.randomUUID())
+    const dispatch = useAppDispatch()
+
+    return (
+        <Tooltip content="Dupliser sykmeldingen">
+            <SlowNextLinkButton
+                href={`/fhir/ny/${nextDraftId.current}`}
+                onClick={() => {
+                    dispatch(nySykmeldingMultistepActions.completeMainStep(dupliserSykmelding(sykmelding)))
+                }}
+                icon={<TabsAddIcon aria-hidden />}
+                variant="tertiary"
+                size="small"
+            />
+        </Tooltip>
+    )
+}
