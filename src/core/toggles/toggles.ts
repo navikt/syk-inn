c693522fde6e52c7db9beec03c81a1a931263558
Karl J. Overå <k@rl.run>
2025-08-06 13:58:39 +0200

    chore: put redacted sykmeldinger behind a feature toggle

--- src/core/toggles/toggles.ts
+++ src/core/toggles/toggles.ts
@@ -4,4 +4,9 @@ export type Toggle = IToggle
 export type Toggles = IToggle[]
 
 export type ExpectedToggles = (typeof EXPECTED_TOGGLES)[number]
-export const EXPECTED_TOGGLES = ['PILOT_USER', 'SYK_INN_REFRESH_TOKEN', 'SYK_INN_AAREG'] as const
+export const EXPECTED_TOGGLES = [
+    'PILOT_USER',
+    'SYK_INN_REFRESH_TOKEN',
+    'SYK_INN_AAREG',
+    'SYK_INN_SHOW_REDACTED',
+] as const
