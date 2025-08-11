8258316901a0d40321cb23a00351348956e40e7f
Karl J. Overå <k@rl.run>
2025-08-11 11:22:39 +0200

    style: align draft actions with sykmelding actions

--- src/features/dashboard/combo-table/draft/DraftActions.tsx
+++ src/features/dashboard/combo-table/draft/DraftActions.tsx
@@ -8,7 +8,7 @@ import { DeleteDraftButton } from './DeleteDraftButton'
 
 export function DraftActions({ draftId }: { draftId: string }): ReactElement {
     return (
-        <div className="grid grid-cols-3 gap-2 w-fit ml-2">
+        <div className="grid grid-flow-col auto-cols-max gap-2">
             <Tooltip content="Åpne utkast">
                 <SlowNextLinkButton
                     href={`/fhir/ny/${draftId}`}
