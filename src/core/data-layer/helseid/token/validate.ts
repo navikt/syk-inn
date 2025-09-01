2317f1689acad99f79bcb99c06ab528312043679
Karl J. Overå <k@rl.run>
2025-09-01 10:05:08 +0200

    chore: otel span up helse id token validation

--- src/core/data-layer/helseid/token/validate.ts
+++ src/core/data-layer/helseid/token/validate.ts
@@ -1,4 +1,4 @@
-import { jwtVerify } from 'jose'
+import { jwtVerify, errors } from 'jose'
 import { logger } from '@navikt/next-logger'
 
 import { getHelseIdWellKnown } from '@data-layer/helseid/token/well-known'
@@ -7,7 +7,7 @@ import { spanServerAsync } from '@lib/otel/server'
 import { getHelseIdAccessToken } from '@data-layer/helseid/token/tokens'
 
 export async function validateHelseIdToken(): Promise<boolean> {
-    return spanServerAsync('HelseID.token-validation', async () => {
+    return spanServerAsync('HelseID.token-validation', async (span) => {
         const accessToken = await getHelseIdAccessToken()
 
         try {
@@ -19,9 +19,21 @@ export async function validateHelseIdToken(): Promise<boolean> {
                 algorithms: ['RS256'],
             })
 
+            span.setAttributes({
+                'HelseID.token.valid': true,
+            })
+
             return true
         } catch (e) {
+            const errorType = e instanceof errors.JOSEError ? e.code : 'UnknownError'
+
             logger.warn(new Error(`HelseID-token validation failed`, { cause: e }))
+
+            span.setAttributes({
+                'HelseID.token.valid': true,
+                'HelseID.token.error-type': errorType,
+            })
+
             return false
         }
     })
