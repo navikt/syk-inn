0ed30c45eaea22b38fb0e98418c9364c6ef10faf
Karl J. Overå <k@rl.run>
2025-09-08 15:30:06 +0200

    chore: fix minor dev-only log lie

--- src/core/toggles/dev/cookie-override.ts
+++ src/core/toggles/dev/cookie-override.ts
@@ -9,15 +9,16 @@ export async function developmentTogglesWithCookieOverrides(): Promise<IToggle[]
     const cookieStore = await cookies()
     const localDevelopmentCookiesWithOverrides = localDevelopmentToggles.map((it) => {
         const enabledByCookieOrNull = cookieStore.get(it.name)?.value.includes('true') ?? null
+
         return {
             ...it,
             enabled: enabledByCookieOrNull ?? it.enabled,
-            overriden: (enabledByCookieOrNull ?? false) !== it.enabled,
+            status: enabledByCookieOrNull ? 'override' : 'config',
         }
     })
 
     const toggleStatus = localDevelopmentCookiesWithOverrides
-        .map((it) => `\t${it.name}: ${it.enabled}${it.overriden ? ' (overridden)' : ''}`)
+        .map((it) => `\t${it.name}: ${it.enabled} (${it.status})`)
         .join('\n')
 
     unleashLogger.warn(`Runtime env is ${bundledEnv.runtimeEnv}, using dev toggles, current toggles: \n${toggleStatus}`)
