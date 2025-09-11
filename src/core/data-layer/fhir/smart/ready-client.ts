804fa554f48632f18b479b73b2870c5496f3947b
Karl J. Overå <k@rl.run>
2025-09-11 12:21:54 +0200

    refactor: move ReadyClient initialization in resolvers to the context

--- src/core/data-layer/fhir/smart/ready-client.ts
+++ src/core/data-layer/fhir/smart/ready-client.ts
@@ -1,10 +1,6 @@
 import { ReadyClient, SmartClientReadyErrors } from '@navikt/smart-on-fhir/client'
-import { FhirPractitioner } from '@navikt/smart-on-fhir/zod'
 import { logger } from '@navikt/next-logger'
-import { GraphQLError } from 'graphql/error'
 
-import { NoSmartSession } from '@data-layer/fhir/error/Errors'
-import { isDemo, isLocal } from '@lib/env'
 import { getActivePatient } from '@data-layer/fhir/smart/active-patient'
 
 import { HelseIdClaimSchema } from './helseid'
@@ -26,51 +22,25 @@ export async function getReadyClient(opts: { validate: true }): Promise<ReadyCli
     return readyClient
 }
 
-export async function getReadyClientForResolvers(): Promise<[ReadyClient]>
-export async function getReadyClientForResolvers(params: {
-    withPractitioner: true
-    validateHelseId?: boolean
-}): Promise<[ReadyClient, FhirPractitioner]>
-export async function getReadyClientForResolvers(params?: {
-    withPractitioner: true
-    validateHelseId?: boolean
-}): Promise<[ReadyClient] | [ReadyClient, FhirPractitioner]> {
-    const client = await getReadyClient({ validate: true })
-    if ('error' in client) {
-        throw NoSmartSession()
-    }
+export async function shadowVerifyHelseIdOnFhir(client: ReadyClient): Promise<void> {
+    try {
+        const helseIdClaim = client.getClaim('https://helseid.nhn.no', HelseIdClaimSchema)
 
-    if (params == null) {
-        return [client]
-    }
-
-    if (params.validateHelseId && (isLocal || isDemo)) {
-        try {
-            const helseIdClaim = client.getClaim('https://helseid.nhn.no', HelseIdClaimSchema)
-
-            if ('error' in helseIdClaim) {
-                logger.warn(
-                    `Smart session was attempted to be launched with HelseID claim, but client says: ${helseIdClaim.error}`,
-                )
-            } else {
-                logger.info(
-                    `Smart session is launched with HelseID claim, and access_token of length ${helseIdClaim.access_token.length} was found`,
-                )
-            }
-        } catch (e) {
+        if ('error' in helseIdClaim) {
             logger.warn(
-                new Error(
-                    'HelseID token validation is only in proof-of-concept mode, this is expected in demo/local, ignoring claim error',
-                    { cause: e },
-                ),
+                `Smart session was attempted to be launched with HelseID claim, but client says: ${helseIdClaim.error}`,
+            )
+        } else {
+            logger.info(
+                `Smart session is launched with HelseID claim, and access_token of length ${helseIdClaim.access_token.length} was found`,
             )
         }
+    } catch (e) {
+        logger.warn(
+            new Error(
+                'HelseID token validation is only in proof-of-concept mode, this is expected in demo/local, ignoring claim error',
+                { cause: e },
+            ),
+        )
     }
-
-    const practitioner = await client.user.request()
-    if ('error' in practitioner) {
-        throw new GraphQLError('PARSING_ERROR')
-    }
-
-    return [client, practitioner]
 }
