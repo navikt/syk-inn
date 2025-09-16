0c21f792d6cd5bbc578b5928f8253e52965809bf
Karl J. Overå <k@rl.run>
2025-09-16 09:50:25 +0200

    fix: loading state on dashboard

--- src/features/dashboard/ComboTableCard.tsx
+++ src/features/dashboard/ComboTableCard.tsx
@@ -15,13 +15,17 @@ function ComboTableCard({ className }: { className?: string }): ReactElement {
     const dashboardQuery = useQuery(AllDashboardDocument)
     useOnFocus(dashboardQuery.refetch)
 
+    const hasDrafts = !!dashboardQuery.data?.drafts?.length
+    const hasSykmeldinger = !!dashboardQuery.data?.sykmeldinger?.length
+    const hasData = hasSykmeldinger || hasDrafts
+
     return (
         <DashboardCard
             className={className}
             ariaLabel="Tidligere sykmeldinger og utkast"
             fetching={dashboardQuery.loading && dashboardQuery.dataState !== 'empty'}
         >
-            {dashboardQuery.dataState === 'complete' && (
+            {hasData && dashboardQuery.data && (
                 <ComboTable
                     sykmeldinger={dashboardQuery.data.sykmeldinger ?? []}
                     drafts={dashboardQuery.data.drafts ?? []}
@@ -30,10 +34,9 @@ function ComboTableCard({ className }: { className?: string }): ReactElement {
                     {dashboardQuery.error && !dashboardQuery.data.sykmeldinger == null && <AllSykmeldingerError />}
                 </ComboTable>
             )}
-            {dashboardQuery.loading && dashboardQuery.dataState !== 'complete' && <ComboTableSkeleton />}
-            {dashboardQuery.dataState === 'complete' &&
-                !dashboardQuery.data.sykmeldinger?.length &&
-                !dashboardQuery.data.drafts!.length && <ComboTableEmptyState />}
+            {dashboardQuery.loading && !hasData && <ComboTableSkeleton />}
+            {!dashboardQuery.loading && !dashboardQuery.error && !hasData && <ComboTableEmptyState />}
+            {!dashboardQuery.loading && dashboardQuery.error && !hasData && <EverythingError />}
         </DashboardCard>
     )
 }
@@ -75,6 +78,29 @@ function AllSykmeldingerError(): ReactElement {
                 </BodyShort>
             </Alert>
         </ComboTableFullCell>
+    )
+}
+
+function EverythingError(): ReactElement {
+    return (
+        <DashboardTable>
+            <ComboTableHeader className="text-text-subtle" />
+            <Table.Body className="text-text-subtle">
+                <Table.Row>
+                    <ComboTableFullCell className="flex flex-col gap-6 items-center justify-center w-full h-64">
+                        <Alert variant="error" className="max-w-prose">
+                            <Heading size="medium" level="3" spacing>
+                                Ukjent feil ved henting av sykmeldinger og utkast
+                            </Heading>
+                            <BodyShort spacing>
+                                Det skjedde en ukjent feil under henting av dine utkast. Du kan prøve å laste siden på
+                                nytt, eller prøve på nytt senere.
+                            </BodyShort>
+                        </Alert>
+                    </ComboTableFullCell>
+                </Table.Row>
+            </Table.Body>
+        </DashboardTable>
     )
 }
 
