156a588756e6c5f5a4fb40e830c52c0b4ff46f02
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2025-09-26 14:39:07 +0200

    feat: broken commit, will fix after holiday

--- src/features/actions/ny-sykmelding/NySykmelding.tsx
+++ src/features/actions/ny-sykmelding/NySykmelding.tsx
@@ -4,13 +4,17 @@ import React, { ReactElement } from 'react'
 import { useQuery } from '@apollo/client/react'
 
 import { LoadablePageHeader } from '@components/layout/Page'
-import { PasientDocument } from '@queries'
+import { AllSykmeldingerDocument, PasientDocument } from '@queries'
 import { useAppSelector } from '@core/redux/hooks'
 import NySykmeldingPageSteps from '@features/ny-sykmelding-form/NySykmeldingPageSteps'
 import { useDiagnoseSuggestions } from '@features/ny-sykmelding-form/diagnose/useDiagnoseSuggestions'
 import { createDefaultFormValues } from '@features/ny-sykmelding-form/form-default-values'
 import NySykmeldingForm from '@features/ny-sykmelding-form/NySykmeldingForm'
 import NySykmeldingFormSkeleton from '@features/ny-sykmelding-form/NySykmeldingFormSkeleton'
+import {
+    hasAnsweredUtdypendeSporsmal,
+    mapSykmeldingToDateRanges,
+} from '@features/dashboard/dumb-stats/continuous-sykefravaer-utils'
 
 function NySykmelding(): ReactElement {
     const { data } = useQuery(PasientDocument)
@@ -27,8 +31,9 @@ function NySykmelding(): ReactElement {
 function NySykmeldingFormWithDefaultValues(): ReactElement {
     const suggestionsQuery = useDiagnoseSuggestions()
     const valuesInState = useAppSelector((state) => state.nySykmelding.values)
+    const alleSykmeldinger = useQuery(AllSykmeldingerDocument)
 
-    if (suggestionsQuery.loading) {
+    if (suggestionsQuery.loading || alleSykmeldinger.loading) {
         return <NySykmeldingFormSkeleton />
     }
 
@@ -38,9 +43,15 @@ function NySykmeldingFormWithDefaultValues(): ReactElement {
         draftValues: null,
     })
 
+    const previousSykmeldingDateRange = mapSykmeldingToDateRanges(alleSykmeldinger.data?.sykmeldinger ?? [])
+
     return (
         <NySykmeldingForm
             defaultValues={defaultValues}
+            context={{
+                previousSykmeldingDateRange,
+                hasAnsweredUtdypendeSporsmal: hasAnsweredUtdypendeSporsmal(alleSykmeldinger.data?.sykmeldinger ?? []),
+            }}
             contextualErrors={{ diagnose: suggestionsQuery.suggestions.diagnose.error }}
         />
     )
