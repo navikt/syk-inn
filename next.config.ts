5f9dae4a01d36d629bda8414e4092428d72fbcd9
Karl J. Overå <k@rl.run>
2025-11-03 13:10:57 +0100

    chore: try and fix module tracing

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,12 @@ const nextConfig: NextConfig = {
     output: 'standalone',
     assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-    transpilePackages: ['@navikt/fhir-mock-server'],
+    transpilePackages: ['@navikt/fhir-mock-server', '@navikt/helseid-mock-server'],
     serverExternalPackages: ['@navikt/next-logger', 'next-logger', 'pino', 'pino-socket', '@whatwg-node'],
+    // NextJS module tracer weridly doesn't include this direct dependency of Pino
+    outputFileTracingIncludes: {
+        '/': ['real-require'],
+    },
     experimental: {
         optimizePackageImports: ['@navikt/ds-react', '@navikt/aksel-icons'],
         authInterrupts: true,
