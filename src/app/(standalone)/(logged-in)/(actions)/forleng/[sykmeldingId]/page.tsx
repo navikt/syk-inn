5063b934c6cf6670e4875218a56b9a3e4f86a5f9
Karl J. Overå <k@rl.run>
2025-11-19 11:04:18 +0100

    feat: implement forlengelse in HelseID-mode

---
+++ src/app/(standalone)/(logged-in)/(actions)/forleng/[sykmeldingId]/page.tsx
@@ -0,0 +1,17 @@
+import React, { ReactElement } from 'react'
+
+import { ForlengSykmeldingFormWithDefaultValues } from '@features/actions/forleng-sykmelding/ForlengSykmelding'
+import NySykmeldingPageSteps from '@features/ny-sykmelding-form/NySykmeldingPageSteps'
+import { StaticPageHeading } from '@components/layout/Page'
+
+async function Page({ params }: PageProps<'/fhir/forleng/[sykmeldingId]'>): Promise<ReactElement> {
+    const sykmeldingId = (await params).sykmeldingId
+
+    return (
+        <NySykmeldingPageSteps heading={<StaticPageHeading>Ny sykmelding (forlengelse)</StaticPageHeading>}>
+            <ForlengSykmeldingFormWithDefaultValues sykmeldingId={sykmeldingId} />
+        </NySykmeldingPageSteps>
+    )
+}
+
+export default Page
