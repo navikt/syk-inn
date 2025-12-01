6946cb576cdc1f8d3a74c3cd24240fe7c5e90041
Karl J. Overå <k@rl.run>
2025-12-01 15:05:03 +0100

    fix: use same logic for 'current'-offset everywhere

--- src/core/data-layer/common/sykmelding-utils.ts
+++ src/core/data-layer/common/sykmelding-utils.ts
@@ -3,6 +3,12 @@ import { isAfter, isSameDay, sub, subDays } from 'date-fns'
 
 import { raise } from '@lib/ts'
 
+/**
+ * The definition of what the offset where Practitioners are able to see the sykmelding without
+ * requesting access to more details.
+ */
+export const PREVIOUS_OFFSET_DAYS = 4
+
 export function byCurrentOrPreviousWithOffset(sykmelding: { values: { aktivitet: { tom: string }[] } }): boolean {
     const latestPeriode = R.firstBy(sykmelding.values.aktivitet, [(it) => it.tom, 'desc'])
 
@@ -10,7 +16,7 @@ export function byCurrentOrPreviousWithOffset(sykmelding: { values: { aktivitet:
         raise('Sykmelding without aktivitetsperioder, this should not happen')
     }
 
-    const countsAsCurrent = subDays(new Date(), 4)
+    const countsAsCurrent = subDays(new Date(), PREVIOUS_OFFSET_DAYS)
     return isSameDay(latestPeriode.tom, countsAsCurrent) || isAfter(latestPeriode.tom, countsAsCurrent)
 }
 
