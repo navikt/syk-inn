7848d14e82f1a32bcb898bacd6d353fba38e5f6e
Karl J. Overå <k@rl.run>
2026-01-15 15:02:47 +0100

    chore: also show :angeryer-erik:-error for bad practitioner

--- src/app/(fhir)/fhir/(fhir-launched)/[patientId]/launched-errors.tsx
+++ src/app/(fhir)/fhir/(fhir-launched)/[patientId]/launched-errors.tsx
@@ -40,6 +40,9 @@ export function NoPractitionerSession(): ReactElement {
                     )}
                 </div>
             </div>
+            {isDevGcp && (
+                <DevGcpErrorInfo why="Dette kan være at man har blitt logget ut, men det kan også være krasj/krøll i FHIR serveren." />
+            )}
         </PageBlock>
     )
 }
@@ -56,38 +59,43 @@ export function NoValidPatient(): ReactElement {
                     i ditt journalsystem (FNR eller DNR).
                 </BodyShort>
             </div>
-            {(isDevGcp || true) && (
-                <div className="max-w-prose mt-8 bg-bg-default p-4 rounded-xl">
-                    <Heading level="3" size="small" spacing className="flex flex-row gap-3 items-center -mt-1">
-                        <TerminalIcon aria-hidden />
-                        Teknisk debug for dev-gcp
-                    </Heading>
-                    <BodyShort spacing>
-                        Dette betyr at FHIR API-et klarte ikke å gi oss en gyldig pasient. Mest sannsynligvis en intern
-                        feil hos EPJ, eventuelt ugyldig struktur (mindre sannsynlig).
-                    </BodyShort>
-                    <BodyShort spacing>
-                        For å se nøyaktig hva som gikk galt, kan du enten sjekke loggene eller OTEL-tracing. Lenkene
-                        under må <span className="font-bold">KOPIERES</span> og åpnes på din faktiske maskin, det er
-                        ikke vits å åpne den i WinVerify1.
-                    </BodyShort>
-                    <List>
-                        <List.Item>
-                            <span>Logger: </span>
-                            <a href="https://grafana.nav.cloud.nais.io/goto/QYbT-ZSvg?orgId=1">Kopier meg</a>
-                            <span> (syk-inn dashboard i dev)</span>
-                        </List.Item>
-                        <List.Item>
-                            <span>Traces: </span>
-                            <a href="https://grafana.nav.cloud.nais.io/goto/GcvoaZSDg?orgId=1">Kopier meg</a>
-                            <span> (error traces de siste 60 minuttene)</span>
-                        </List.Item>
-                    </List>
-                    <SessionIdInfo>
-                        <BodyShort>(Mest sannsynligvis ikke relevant for denne feilen)</BodyShort>
-                    </SessionIdInfo>
-                </div>
+            {isDevGcp && (
+                <DevGcpErrorInfo why="Dette betyr at FHIR API-et klarte ikke å gi oss en gyldig pasient. Mest sannsynligvis en intern feil hos EPJ, eventuelt ugyldig struktur (mindre sannsynlig)." />
             )}
         </PageBlock>
+    )
+}
+
+function DevGcpErrorInfo({ why }: { why: string }): ReactElement | null {
+    if (!isDevGcp) return null
+
+    return (
+        <div className="max-w-prose mt-8 bg-bg-default p-4 rounded-xl">
+            <Heading level="3" size="small" spacing className="flex flex-row gap-3 items-center -mt-1">
+                <TerminalIcon aria-hidden />
+                Teknisk debug for dev-gcp
+            </Heading>
+            <BodyShort spacing>{why}</BodyShort>
+            <BodyShort spacing>
+                For å se nøyaktig hva som gikk galt, kan du enten sjekke loggene eller OTEL-tracing. Lenkene under må{' '}
+                <span className="font-bold">KOPIERES</span> og åpnes på din faktiske maskin, det er ikke vits å åpne den
+                i WinVerify1.
+            </BodyShort>
+            <List>
+                <List.Item>
+                    <span>Logger: </span>
+                    <a href="https://grafana.nav.cloud.nais.io/goto/QYbT-ZSvg?orgId=1">Kopier meg</a>
+                    <span> (syk-inn dashboard i dev)</span>
+                </List.Item>
+                <List.Item>
+                    <span>Traces: </span>
+                    <a href="https://grafana.nav.cloud.nais.io/goto/GcvoaZSDg?orgId=1">Kopier meg</a>
+                    <span> (error traces de siste 60 minuttene)</span>
+                </List.Item>
+            </List>
+            <SessionIdInfo>
+                <BodyShort>(Mest sannsynligvis ikke relevant for denne feilen)</BodyShort>
+            </SessionIdInfo>
+        </div>
     )
 }
