894147fcff45dec0eeee61859c5d04b0f281f7d0
Karl J. Overå <k@rl.run>
2026-02-10 12:05:43 +0100

    refactor: use aksel's combobox istead of react-aria

--- src/components/form/diagnose-combobox/DiagnoseCombobox.tsx
+++ src/components/form/diagnose-combobox/DiagnoseCombobox.tsx
@@ -1,26 +1,14 @@
-import * as R from 'remeda'
-import React, { ReactElement, ReactNode, startTransition } from 'react'
-import { BodyShort, Button, Detail, ErrorMessage, Label } from '@navikt/ds-react'
+import React, { ReactElement, ReactNode, useState } from 'react'
+import { BodyShort, Button, Detail, Label, UNSAFE_Combobox as Combobox } from '@navikt/ds-react'
 import { useQuery } from '@apollo/client/react'
+import * as R from 'remeda'
 
-import { cn } from '@lib/tw'
-import { raise } from '@lib/ts'
-import { DiagnoseFragment, DiagnoseSearchDocument } from '@queries'
 import { Diagnose, DiagnoseSystem } from '@data-layer/common/diagnose'
-import { SimpleAlert } from '@components/help/GeneralErrors'
+import { DiagnoseFragment, DiagnoseSearchDocument } from '@queries'
+import { raise } from '@lib/ts'
+import { cn } from '@lib/tw'
 
-import {
-    AkselifiedCombobox,
-    AkselifiedComboboxDisclosure,
-    AkselifiedComboboxItem,
-    AkselifiedComboboxLoading,
-    AkselifiedComboboxNonInteractiveFeedbackItem,
-    AkselifiedComboboxNonSelectables,
-    AkselifiedComboboxPopover,
-    AkselifiedComboboxWrapper,
-    useComboboxStore,
-    useStoreState,
-} from './AkselifiedCombobox'
+import styles from './DiagnoseCombobox.module.css'
 
 interface Props {
     id?: string
@@ -47,99 +35,61 @@ function DiagnoseCombobox({
     error,
     actions,
 }: Props): ReactElement {
-    const combobox = useComboboxStore({
-        defaultValue: value ? createUniqueValue(value) : '',
-        selectedValue: value ? createUniqueValue(value) : '',
-        setSelectedValue: (value) => {
-            const unwrappedValue = unwrapUniqueValue(value)
-            const selectedSuggestion = suggestions.find(
-                (it) =>
-                    it.code.toLowerCase() === unwrappedValue.code.toLowerCase() &&
-                    it.system.toLowerCase() === unwrappedValue.system.toLowerCase(),
-            )
+    const comboRef = React.useRef<HTMLInputElement>(null)
+    const endreRef = React.useRef<HTMLButtonElement>(null)
+    const [overwrite, setOverwrite] = useState(false)
 
-            if (selectedSuggestion == null) {
-                raise("Illegal state: Selected suggestion doesn't match with cache")
-            }
-
-            onSelect(R.omit(selectedSuggestion, ['__typename']))
-
-            document.getElementById('step-navigation-next')?.focus()
-        },
-        setValue: () => {
-            startTransition(() => {
-                onChange()
-            })
-        },
+    const [query, setQuery] = useState(value ? value.code : '')
+    const { isLoading, hasError, suggestions } = useSuggestions(query)
+    const serverSuggestionOptions = suggestions.map((it) => {
+        return {
+            label: `${it.text} - ${it.code} \n${it.system}`,
+            value: createUniqueValue(it),
+        }
     })
-    const state = useStoreState(combobox)
-    const { isLoading, hasError, suggestions } = useSuggestions(state.value)
+
+    const handleOnToggleSelected = (value: string): void => {
+        const unwrappedValue = unwrapUniqueValue(value)
+        const selectedSuggestion = suggestions.find(
+            (it) =>
+                it.code.toLowerCase() === unwrappedValue.code.toLowerCase() &&
+                it.system.toLowerCase() === unwrappedValue.system.toLowerCase(),
+        )
+
+        if (selectedSuggestion == null) {
+            raise("Illegal state: Selected suggestion doesn't match with cache")
+        }
+
+        onSelect(R.omit(selectedSuggestion, ['__typename']))
+        setOverwrite(false)
+
+        requestAnimationFrame(() => {
+            endreRef.current?.focus()
+        })
+    }
 
     return (
         <fieldset className={cn('p-0', className)}>
             <legend className="sr-only">{label}</legend>
-            <div
-                className={cn({
-                    hidden: value != null,
-                })}
-            >
-                <AkselifiedComboboxWrapper labelId={`${id}-label`} label={label} className={className} store={combobox}>
-                    {description && <BodyShort className="navds-form-field__description">{description}</BodyShort>}
-                    <AkselifiedCombobox
-                        id={id}
-                        aria-labelledby={`${id}-label`}
-                        placeholder="Søk på kode eller beskrivelse"
-                        onBlur={onBlur}
-                        error={error}
-                        autoFocus
-                    >
-                        <AkselifiedComboboxDisclosure loading={suggestions.length > 0 && isLoading} />
-                    </AkselifiedCombobox>
-                    <AkselifiedComboboxPopover>
-                        <AkselifiedComboboxNonSelectables>
-                            {suggestions.length === 0 && isLoading && <AkselifiedComboboxLoading />}
-                            {(state.value.trim() === '' || (suggestions.length === 0 && !isLoading && !hasError)) && (
-                                <AkselifiedComboboxNonInteractiveFeedbackItem>
-                                    Søk på enten kode eller beskrivelse
-                                </AkselifiedComboboxNonInteractiveFeedbackItem>
-                            )}
-                            {suggestions.length === 0 && !isLoading && state.value.trim() !== '' && !hasError && (
-                                <AkselifiedComboboxNonInteractiveFeedbackItem>
-                                    {`Fant ingen diagnose med kode eller beskrivelse "${state.value}"`}
-                                </AkselifiedComboboxNonInteractiveFeedbackItem>
-                            )}
-                            {hasError && (
-                                <SimpleAlert
-                                    level="error"
-                                    title={`En feil skjedde ved søk etter diagnoser på "${state.value}"`}
-                                    noSessionId
-                                    noCallToAction
-                                >
-                                    Vennligst prøv igjen senere.
-                                </SimpleAlert>
-                            )}
-                        </AkselifiedComboboxNonSelectables>
-                        {state.value &&
-                            suggestions.length > 0 &&
-                            suggestions.map((value) => (
-                                <AkselifiedComboboxItem
-                                    key={`${value.code}-${value.system}`}
-                                    value={createUniqueValue(value)}
-                                >
-                                    <div>
-                                        <Label>{value.code}</Label>
-                                        <Detail>{value.system}</Detail>
-                                    </div>
-                                    <Detail className="text-right break-words ml-2 max-w-48 overflow-hidden">
-                                        {value.text}
-                                    </Detail>
-                                </AkselifiedComboboxItem>
-                            ))}
-                    </AkselifiedComboboxPopover>
-                    {error && <ErrorMessage>{error}</ErrorMessage>}
-                    <div className="absolute top-0 right-0">{actions}</div>
-                </AkselifiedComboboxWrapper>
-            </div>
+            {(value == null || overwrite) && (
+                <Combobox
+                    ref={comboRef}
+                    id={id}
+                    label={label}
+                    description={description}
+                    className={cn(styles.comboboxWrapper, className)}
+                    value={query}
+                    onChange={setQuery}
+                    isLoading={isLoading}
+                    isListOpen={!!query.trim() && query.length > 0}
+                    error={hasError ? 'Klarte ikke å søke i diagnoser, prøv igjen senere' : error}
+                    onBlur={onBlur}
+                    filteredOptions={serverSuggestionOptions}
+                    options={serverSuggestionOptions}
+                    onToggleSelected={handleOnToggleSelected}
+                    placeholder="Start å skrive for å søke etter diagnosekode"
+                />
+            )}
             {value != null && (
                 <div className="flex justify-between">
                     <div>
@@ -149,18 +99,21 @@ function DiagnoseCombobox({
                         </BodyShort>
                         <Detail spacing>{value.system}</Detail>
                     </div>
-                    <div className="flex gap-1 items-start">
+                    <div className="flex gap-2 items-start">
                         <Button
+                            ref={endreRef}
                             data-color="neutral"
                             variant="secondary"
                             size="small"
                             type="button"
                             onClick={() => {
-                                const resetValue = state.selectedValue
-                                    ? unwrapUniqueValue(state.selectedValue).code
-                                    : ''
-                                combobox.setValue(resetValue)
+                                setQuery(value?.code)
+                                setOverwrite(true)
                                 onChange()
+
+                                requestAnimationFrame(() => {
+                                    comboRef.current?.focus()
+                                })
                             }}
                         >
                             Endre
