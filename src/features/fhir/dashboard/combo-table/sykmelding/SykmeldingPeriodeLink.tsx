294bdbf2aef86c5636c11dc7982709a7c992b445
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2026-02-17 11:00:50 +0100

    feat: show sykmeldingsperioder and grad in helptext

--- src/features/fhir/dashboard/combo-table/sykmelding/SykmeldingPeriodeLink.tsx
+++ src/features/fhir/dashboard/combo-table/sykmelding/SykmeldingPeriodeLink.tsx
@@ -1,6 +1,6 @@
 import * as R from 'remeda'
 import React, { ReactElement } from 'react'
-import { BodyShort, VStack } from '@navikt/ds-react'
+import { BodyShort, HelpText } from '@navikt/ds-react'
 
 import { SykmeldingFragment } from '@queries'
 import { AkselNextLink } from '@components/links/AkselNextLink'
@@ -18,19 +18,22 @@ function SykmeldingPeriodeLink({ sykmeldingId, aktivitet }: Props): ReactElement
     const sortedPeriode = R.sortBy(aktivitet, [(it) => it.fom, 'asc'])
 
     return (
-        <VStack gap="space-4">
+        <div className="flex gap-2">
             <AkselNextLink href={mode.paths.sykmelding(sykmeldingId)}>
                 {sykmeldingPeriodeText(sortedPeriode)}
             </AkselNextLink>
-            {sortedPeriode.length > 1 &&
-                sortedPeriode.map((periode) => (
-                    <React.Fragment key={periode.fom}>
-                        <BodyShort size="small" className="whitespace-nowrap">
-                            {sykmeldingPeriodeText([periode])} ({aktivitetGradText(periode)})
-                        </BodyShort>
-                    </React.Fragment>
-                ))}
-        </VStack>
+            {sortedPeriode.length > 1 && (
+                <HelpText title="Se alle perioder for sykmelding">
+                    {sortedPeriode.map((periode) => (
+                        <React.Fragment key={periode.fom}>
+                            <BodyShort size="small" className="whitespace-nowrap">
+                                {sykmeldingPeriodeText([periode])} ({aktivitetGradText(periode)})
+                            </BodyShort>
+                        </React.Fragment>
+                    ))}
+                </HelpText>
+            )}
+        </div>
     )
 }
 
