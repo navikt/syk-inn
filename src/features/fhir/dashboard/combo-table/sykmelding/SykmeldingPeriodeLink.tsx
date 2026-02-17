10cb32c6257fbc26695dfcb652f0767512d02e7e
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2026-02-17 10:26:25 +0100

    chore: show all perioder in title

--- src/features/fhir/dashboard/combo-table/sykmelding/SykmeldingPeriodeLink.tsx
+++ src/features/fhir/dashboard/combo-table/sykmelding/SykmeldingPeriodeLink.tsx
@@ -6,7 +6,7 @@ import { SykmeldingFragment } from '@queries'
 import { AkselNextLink } from '@components/links/AkselNextLink'
 import { useMode } from '@core/providers/Modes'
 
-import { sykmeldingPeriodeText } from './sykmelding-utils'
+import { aktivitetGradText, sykmeldingPeriodeText } from './sykmelding-utils'
 
 type Props = {
     sykmeldingId: string
@@ -15,16 +15,21 @@ type Props = {
 
 function SykmeldingPeriodeLink({ sykmeldingId, aktivitet }: Props): ReactElement {
     const mode = useMode()
-    const [first, ...rest] = R.sortBy(aktivitet, [(it) => it.fom, 'desc'])
+    const sortedPeriode = R.sortBy(aktivitet, [(it) => it.fom, 'asc'])
 
     return (
-        <VStack gap="space-2">
-            <AkselNextLink href={mode.paths.sykmelding(sykmeldingId)}>{sykmeldingPeriodeText(first)}</AkselNextLink>
-            {rest.map((periode) => (
-                <BodyShort size="small" key={periode.fom}>
-                    {sykmeldingPeriodeText(periode)}
-                </BodyShort>
-            ))}
+        <VStack gap="space-4">
+            <AkselNextLink href={mode.paths.sykmelding(sykmeldingId)}>
+                {sykmeldingPeriodeText(sortedPeriode)}
+            </AkselNextLink>
+            {sortedPeriode.length > 1 &&
+                sortedPeriode.map((periode) => (
+                    <React.Fragment key={periode.fom}>
+                        <BodyShort size="small" className="whitespace-nowrap">
+                            · {sykmeldingPeriodeText([periode])} ({aktivitetGradText(periode)})
+                        </BodyShort>
+                    </React.Fragment>
+                ))}
         </VStack>
     )
 }
--- src/features/fhir/dashboard/combo-table/sykmelding/sykmelding-utils.ts
+++ src/features/fhir/dashboard/combo-table/sykmelding/sykmelding-utils.ts
@@ -1,13 +1,34 @@
 import { raise } from '@lib/ts'
 import { toReadableDatePeriod } from '@lib/date'
-import { AktivitetFragment, DiagnoseFragment } from '@queries'
+import { AktivitetFragment, DiagnoseFragment, AktivitetRedacted } from '@queries'
+import { earliestFom, latestTom } from '@data-layer/common/sykmelding-utils'
 
-export function sykmeldingPeriodeText(periode: { fom: string; tom: string }): string {
-    if (!periode.fom || !periode.tom) {
+export function sykmeldingPeriodeText(perioder: { fom: string; tom: string }[]): string {
+    const earliestPeriode = earliestFom({ values: { aktivitet: perioder } })
+    const latestPeriode = latestTom({ values: { aktivitet: perioder } })
+
+    if (!earliestPeriode || !latestPeriode) {
         raise('Sykmelding without aktivitetsperioder, this should not happen')
     }
 
-    return toReadableDatePeriod(periode.fom, periode.tom)
+    return toReadableDatePeriod(earliestPeriode, latestPeriode)
+}
+
+export function aktivitetGradText(aktivitet: AktivitetFragment | AktivitetRedacted): string {
+    switch (aktivitet.__typename) {
+        case 'AktivitetIkkeMulig':
+            return '100%'
+        case 'Gradert':
+            return `${aktivitet.grad}%`
+        case 'Avventende':
+            return 'Avventende'
+        case 'Reisetilskudd':
+            return 'Reisetilskudd'
+        case 'Behandlingsdager':
+            return 'Behandlingsdager'
+        case 'AktivitetRedacted':
+            return 'Skjult'
+    }
 }
 
 export function sykmeldingDiagnoseText(hoveddiagnose: DiagnoseFragment | null | undefined): string {
@@ -15,20 +36,7 @@ export function sykmeldingDiagnoseText(hoveddiagnose: DiagnoseFragment | null | 
 }
 
 export function sykmeldingGradText(aktivitet: AktivitetFragment[]): string {
-    const [first] = aktivitet
-
-    switch (first.__typename) {
-        case 'AktivitetIkkeMulig':
-            return '100%'
-        case 'Gradert':
-            return `${first.grad}%`
-        case 'Avventende':
-            return 'Avventende'
-        case 'Reisetilskudd':
-            return 'Reisetilskudd'
-        case 'Behandlingsdager':
-            return 'Behandlingsdager'
-    }
+    return aktivitetGradText(aktivitet[0])
 }
 
 export function sykmeldingArbeidsgiverText(
