2cefea9d702509b1f029c484520f26ea15d2c664
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2026-03-12 13:09:25 +0100

    chore: fix uu-issue with ul inside of p

--- src/features/fhir/dashboard/top-card/InfoNySykmeldingCard.tsx
+++ src/features/fhir/dashboard/top-card/InfoNySykmeldingCard.tsx
@@ -12,13 +12,11 @@ export function InfoNySykmeldingCard(): ReactElement {
                     Løsningen er under utvikling, og har derfor litt begrenset funksjonalitet på noen områder. Foreløpig
                     har vi ikke støtte for:
                 </BodyShort>
-                <BodyShort spacing>
-                    <ul className="list-disc pl-6">
-                        <li>Enkeltstående behandlingsdager</li>
-                        <li>avventende sykmelding</li>
-                        <li>reisetilskudd</li>
-                    </ul>
-                </BodyShort>
+                <ul className="list-disc pl-6 mb-3">
+                    <li>Enkeltstående behandlingsdager</li>
+                    <li>avventende sykmelding</li>
+                    <li>reisetilskudd</li>
+                </ul>
                 <BodyShort spacing>
                     For sykmeldinger hvor dette er aktuelt, må du inntil videre bruke gammel/eksisterende/ordinær
                     løsning. Les mer om ny sykmeldingsløsning på{' '}
