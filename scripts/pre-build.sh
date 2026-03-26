dd0258455db873e43d007a605a24591e40a74c21
Karl J. Overå <k@rl.run>
2026-03-26 17:41:30 +0100

    chore: build typst binary into docker image

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -24,3 +24,23 @@ else
   runtimeEnv=${NEXT_PUBLIC_RUNTIME_ENV:-$(grep "^NEXT_PUBLIC_RUNTIME_ENV=" .env.production | cut -d '=' -f2)}
   printf "\e[32m  👍 .env.production already exists. It's all good in the hood... Building application as \e[44;97m $runtimeEnv \e[32m\e[0m\n"
 fi
+
+# Verify that typst binary is correctly loaded, i.e. its available in ./typst-pdf/typst
+if ! command -v ./typst-pdf/typst &> /dev/null; then
+  printf "\e[31mError: typst binary not found in ./typst-pdf/typst. Please ensure it is correctly loaded.\e[0m\n"
+
+  ls -al ./typst-pdf
+
+  exit 1
+else
+  # Verify that typst binary is the same as one defined in .mise.toml
+  typstVersion=$(./typst-pdf/typst --version | awk '{print $2}')
+  miseTypstVersion=$(grep "^typst" .mise.toml | cut -d '"' -f2)
+
+  if [ "$typstVersion" != "$miseTypstVersion" ]; then
+    printf "\e[31mError: typst binary version ($typstVersion) does not match the version defined in .mise.toml ($miseTypstVersion). Please ensure they are the same.\e[0m\n"
+    exit 1
+  fi
+
+  printf "\e[32m  👍 typst binary ($typstVersion/$miseTypstVersion) found in ./typst-pdf/typst. All good!\e[0m\n"
+fi
--- src/core/pdf/typst-service.ts
+++ src/core/pdf/typst-service.ts
@@ -1,30 +1,47 @@
 import { execFile } from 'node:child_process'
 import { promisify } from 'node:util'
 
+import { logger } from '@navikt/next-logger'
+
 import type { SykInnApiSykmelding } from '@core/services/syk-inn-api/schema/sykmelding'
+import { failSpan, spanServerAsync } from '@lib/otel/server'
 
 const execFileAsync = promisify(execFile)
 
 type TypstPdfResult = { ok: true; pdf: ArrayBuffer } | { ok: false; error: string }
 
 export async function createTypstSykmelding(data: SykInnApiSykmelding): Promise<TypstPdfResult> {
-    try {
-        const { stdout } = await execFileAsync(
-            'typst',
-            [
-                'compile',
-                '--pdf-standard=a-2a',
-                '--font-path=./typst-pdf/fonts',
-                `--input=data=${JSON.stringify(data)}`,
-                'typst-pdf/sykmelding.typ',
-                '-',
-            ],
-            { encoding: 'buffer' },
-        )
+    return spanServerAsync('typst-service.createTypstSykmelding', async (span) => {
+        try {
+            const start = performance.now()
+            const { stdout } = await execFileAsync(
+                './typst-pdf/typst',
+                [
+                    'compile',
+                    '--pdf-standard=a-2a',
+                    '--font-path=./typst-pdf/fonts',
+                    `--input=data=${JSON.stringify(data)}`,
+                    'typst-pdf/sykmelding.typ',
+                    '-',
+                ],
+                { encoding: 'buffer' },
+            )
 
-        return { ok: true, pdf: stdout.buffer }
-    } catch (e) {
-        const err = e as { stderr?: Buffer; message?: string }
-        return { ok: false, error: err.stderr?.toString() ?? err.message ?? 'Unknown error' }
-    }
+            logger.info(
+                `Seems like PDF generation was OK, ${stdout.buffer.byteLength}, it took ${(performance.now() - start).toFixed(2)}ms`,
+            )
+
+            return { ok: true, pdf: stdout.buffer }
+        } catch (e) {
+            failSpan.silently(span, 'PDF generation with typst', e as Error)
+
+            // Is it ENOENT? In which case, the binary is missing
+            if (e instanceof Error && 'code' in e && e.code === 'ENOENT') {
+                return { ok: false, error: 'Typst binary not found. Ensure it is available in ./typst-pdf/typst' }
+            }
+
+            const err = e as { stderr?: Buffer; message?: string }
+            return { ok: false, error: err.stderr?.toString() ?? err.message ?? 'Unknown error' }
+        }
+    })
 }
