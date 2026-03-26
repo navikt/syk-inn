0a4ce71a34b20936aded7b92b4e4d24a6e2b8da4
Karl J. Overå <k@rl.run>
2026-03-26 14:14:39 +0100

    chore: remove remaining gotenberg leftovers

--- src/features/sykmelding-kvittering/SykmeldingKvittering.tsx
+++ src/features/sykmelding-kvittering/SykmeldingKvittering.tsx
@@ -122,12 +122,6 @@ function SykmeldingKvitteringSummary({ sykmeldingId }: { sykmeldingId: string })
                         <AkselLink href={pathWithBasePath(mode.paths.pdf(sykmelding.sykmeldingId))} target="_blank">
                             Se innsendt dokument
                         </AkselLink>
-                        <AkselLink
-                            href={pathWithBasePath(mode.paths.pdf(sykmelding.sykmeldingId)) + '?gotenberg=true'}
-                            target="_blank"
-                        >
-                            Se innsendt dokument (ny)
-                        </AkselLink>
                     </>
                 ) : (
                     <Skeleton width={240} />
