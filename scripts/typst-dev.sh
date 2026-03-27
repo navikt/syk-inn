0c8acf37452eee6ef863a09e64f4a3cb373a98a9
Karl J. Overå <k@rl.run>
2026-03-27 16:07:14 +0100

    chore: add some simple tooling for initializing typst locally

--- scripts/typst-dev.sh
+++ scripts/typst-dev.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
+# Verify that typst binary is the same as one defined in .mise.toml
+typstVersion=$(./typst-pdf/typst --version | awk '{print $2}')
+miseTypstVersion=$(grep "^typst" .mise.toml | cut -d '"' -f2)
+
+if [ "$typstVersion" != "$miseTypstVersion" ]; then
+  printf "\e[31mError: typst binary version ($typstVersion) does not match the version defined in .mise.toml ($miseTypstVersion). Run yarn init:pdf .\e[0m\n"
+  exit 1
+fi
+
 DATA=$(cat ./typst-pdf/test-data/big.json)
 
 printf "\e[32m  👍 Running typst watch! Open ./typst-pdf/sykmelding.pdf to view changes live!"
