7c3feac33f17dad8e7f898a54dfb7a51c1f93354
Karl J. Overå <k@rl.run>
2026-03-27 09:50:32 +0100

    chore: add a simple way to develop the .typ template using typst watch (yarn dev:pdf)

--- src/app/dev/debug-typst/route.tsx
+++ src/app/dev/debug-typst/route.tsx
@@ -4,7 +4,13 @@ import { createTypstSykmelding } from '@core/pdf/typst-service'
 import { SykmeldingBuilder } from '@dev/mock-engine/scenarios/SykInnApiSykmeldingBuilder'
 
 export async function GET(): Promise<Response> {
-    const body = await createTypstSykmelding(new SykmeldingBuilder().enkelAktivitet().build())
+    const chonkySykmelding = new SykmeldingBuilder({ offset: -70 })
+        .enkelAktivitet({ offset: 0, days: 7 })
+        .enkelAktivitet({ offset: 8, days: 14 })
+        .uke17Answered()
+        .build()
+
+    const body = await createTypstSykmelding(chonkySykmelding)
     if (!body.ok) {
         logger.error(`Unable to generate PDF, typst says: ${body.error}`)
         return new Response('Internal server error', { status: 500 })
