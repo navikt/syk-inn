5e5171ddb680338e9d068b767e86df1b533f6b58
Karl J. Overå <k@rl.run>
2026-04-08 22:35:58 +0200

    chore: temporarily move PDF tests

--- src/core/pdf/pdf-service.integration.ts
+++ src/core/pdf/pdf-service.integration.ts
@@ -21,7 +21,7 @@ describe('typst CLI integration', () => {
         const { info } = await pdf.getInfo()
 
         expect(info.Title).toEqual('Innsendt sykmelding')
-        expect(info.Author).toEqual('Kari Nordmann Lege (123456)')
+        expect(info.Author).toEqual('Kari Nordmann Legesson (123456)')
         expect(info.Language).toEqual('nb')
     })
 
@@ -47,6 +47,48 @@ describe('typst CLI integration', () => {
                 expect(info.Title).toEqual('Innsendt sykmelding')
             }),
         )
+    })
+
+    // eslint-disable-next-line vitest/expect-expect
+    test('Generating your PDF should work', async () => {
+        // TODO: Previous syk-inn-api integration test, pdf is no longer part of API
+        /*
+        const payload = createFullOpprettSykmeldingPayload()
+        const opprettResult = await sykInnApiService.opprettSykmelding(payload)
+
+        if ('errorType' in opprettResult) {
+            throw Error(`Opprett failed, expected OK but had error: ${opprettResult.errorType}`)
+        }
+
+        const pdf = await sykInnApiService.getSykmeldingPdf(opprettResult.sykmeldingId, payload.meta.sykmelderHpr)
+
+        if ('errorType' in pdf) {
+            throw Error(`Fetch PDF by ID failed, expected OK but had error: ${pdf.errorType}`)
+        }
+
+        expect(pdf.byteLength).toBeGreaterThan(1000)
+         */
+    }, 10_000)
+
+    // eslint-disable-next-line vitest/expect-expect
+    test('Generating PDF that does not belong to HPR should not work', async () => {
+        // TODO: Previous syk-inn-api integration test, pdf is no longer part of API
+        /*
+        const payload = createFullOpprettSykmeldingPayload()
+        const opprettResult = await sykInnApiService.opprettSykmelding(payload)
+
+        if ('errorType' in opprettResult) {
+            throw Error(`Opprett failed, expected OK but had error: ${opprettResult.errorType}`)
+        }
+
+        const pdf = await sykInnApiService.getSykmeldingPdf(opprettResult.sykmeldingId, 'wrong-hpr')
+
+        if (!('errorType' in pdf)) {
+            throw Error(`Expected error when trying to fetch PDF with wrong HPR, but got PDF of size ${pdf.byteLength}`)
+        }
+
+        expect(pdf.errorType).toEqual('SYKMELDING_PDF_FORBIDDEN')
+        */
     })
 })
 
