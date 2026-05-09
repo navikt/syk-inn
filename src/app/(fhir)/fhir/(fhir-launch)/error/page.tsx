4215ccea9e65cad56922b4e34d94d5140024e9f6
Karl J. Overå <k@rl.run>
2026-05-09 10:03:37 +0200

    chore: Revert "add temporary unavailable login page"
    This reverts commit 1611c7711ec0e27257544bd1f705c5eeb1a22385.

--- src/app/(fhir)/fhir/(fhir-launch)/error/page.tsx
+++ src/app/(fhir)/fhir/(fhir-launch)/error/page.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from 'react'
 
-import { CallbackError, InvalidCode, UnknownError, WeUpgradinError } from '../launch-errors'
+import { CallbackError, InvalidCode, UnknownError } from '../launch-errors'
 
 type Props = {
     searchParams: Promise<{
@@ -15,8 +15,6 @@ async function Page({ searchParams }: Props): Promise<ReactElement> {
         return <InvalidCode />
     } else if (launchError === 'callback-failed') {
         return <CallbackError />
-    } else if (launchError === 'oppgradering') {
-        return <WeUpgradinError />
     }
 
     return <UnknownError />
