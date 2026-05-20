248b13777e10fc35be46a9e8cc37741f4c5d1f45
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2026-05-20 14:01:19 +0200

    feat: add logging to whether the access token and id token is available from the smart app.

--- src/core/data-layer/helseid/token/tokens.ts
+++ src/core/data-layer/helseid/token/tokens.ts
@@ -1,14 +1,14 @@
 import { headers } from 'next/headers'
 
 /**
- * Wonderwall (see README.md) exchanges it's own session ID for the actual HelseID access token. This
- * is not available in a other contexts other than a RSC or route handler.
+ * Wonderwall (see README.md) exchanges its own session ID for the actual HelseID access token. This
+ * is not available in another contexts other than an RSC or route handler.
  */
 export async function getHelseIdAccessToken(): Promise<string> {
     const bearerToken = (await headers()).get('Authorization')
 
     if (!bearerToken) {
-        throw new Error('No bearer token found')
+        throw new Error('No HelseID access_token found')
     }
 
     return bearerToken.replace('Bearer ', '')
