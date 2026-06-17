871b2aa26ec326561ddc077ea2258a7d4e797af0
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2026-06-17 09:47:31 +0200

    fix: formatting

--- src/app/(fhir)/fhir/(fhir-launch)/callback/route.ts
+++ src/app/(fhir)/fhir/(fhir-launch)/callback/route.ts
@@ -1,10 +1,10 @@
 import { redirect } from 'next/navigation'
 import { logger as pinoLogger } from '@navikt/next-logger'
 
-import {pathWithBasePath} from "@lib/url";
-import {getSmartClient} from "@data-layer/fhir/smart/smart-client";
-import {getSessionId} from "@core/session/session";
-import {getFlag, getUserlessToggles} from "@core/toggles/unleash";
+import { pathWithBasePath } from '@lib/url'
+import { getSmartClient } from '@data-layer/fhir/smart/smart-client'
+import { getSessionId } from '@core/session/session'
+import { getFlag, getUserlessToggles } from '@core/toggles/unleash'
 import { failSpan, spanServerAsync } from '@lib/otel/server'
 
 const logger = pinoLogger.child({}, { msgPrefix: '[Secure FHIR (callback)] ' })
