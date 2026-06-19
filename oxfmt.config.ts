b146ecb9ce412aa37d60a237675c07fa2a9f3854
Karl J. Overå <k@rl.run>
2026-06-19 12:24:36 +0200

    chore: bump @navikt/smart-on-fhir

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from 'oxfmt'
 
 export default defineConfig({
     ...config,
+    // Disable this until we can fix the oxfmt sort for typescript alias
+    sortImports: undefined,
     ignorePatterns: ['src/features/ny-sykmelding-form/summary/rules/rule-texts.ts'],
 })
