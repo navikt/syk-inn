48284558b8bd6e4ee28ff48731a62ddc7ea802bb
Karl J. Overå <k@rl.run>
2026-09-07 09:37:05 +0200

    chore: remove false comment

--- src/lib/test/syk-inn-api.ts
+++ src/lib/test/syk-inn-api.ts
@@ -11,9 +11,7 @@ const KAFKA_ALIAS = 'kafka'
 /**
  * See README for local run with syk-inn-api-local docker image
  */
-const SYK_INN_API_IMAGE =
-    // TODO: This uses the branch specific image, should be cleaned up before go-live
-    process.env.SYK_INN_API_INTEGRATION_TESTS_IMAGE || 'ghcr.io/navikt/syk-inn-api-test:latest'
+const SYK_INN_API_IMAGE = process.env.SYK_INN_API_INTEGRATION_TESTS_IMAGE || 'ghcr.io/navikt/syk-inn-api-test:latest'
 if (!(SYK_INN_API_IMAGE.startsWith('ghcr.io/navikt') || SYK_INN_API_IMAGE.startsWith('syk-inn-api-local'))) {
     throw new Error("Oop! This image isn't under ghcr.io/navikt, that seems illegal 🤔")
 }
