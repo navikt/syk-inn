a2d31871be8e600b6b3de2ec7a20bdb47962973f
Karl J. Overå <k@rl.run>
2026-09-09 09:40:42 +0200

    fix: demo route should demo in demo, not not demo

--- src/app/demo/route.ts
+++ src/app/demo/route.ts
@@ -1,12 +1,10 @@
 import { notFound, redirect } from 'next/navigation'
 
 import { createFhirScenarioUrl } from '#dev/tools/scenarios/scenario-url-utils'
-import { isDemo, isLocal } from '#lib/env'
+import { isDemo } from '#lib/env'
 
 export function GET(): void {
-    if (!(isLocal && isDemo)) {
-        notFound()
-    }
+    if (!isDemo) notFound()
 
     redirect(createFhirScenarioUrl('demo', 'Espen Eksempel', 'Magnar Koman', 'Magnar Legekontor', false))
 }
