0c29caa80ed120cb2999f5ced409125e751c8376
Karl J. Overå <k@rl.run>
2026-09-10 16:14:37 +0200

    chore: generated private key jwk pair should also have kid [skip ci]

--- scripts/generate-jwk.ts
+++ scripts/generate-jwk.ts
@@ -1,11 +1,13 @@
-import { exportJWK, generateKeyPair } from 'jose'
+import { calculateJwkThumbprint, exportJWK, generateKeyPair } from 'jose'
 
 const alg = 'RS256'
 
 const { privateKey, publicKey } = await generateKeyPair(alg, { extractable: true })
+const publicJwk = await exportJWK(publicKey)
+const kid = await calculateJwkThumbprint(publicJwk)
 
 // private -> stdout, so it can be piped into wl-copy/pbcopy without hitting the scrollback
-process.stdout.write(JSON.stringify({ ...(await exportJWK(privateKey)), alg, use: 'sig' }))
+process.stdout.write(JSON.stringify({ ...(await exportJWK(privateKey)), alg, use: 'sig', kid }))
 
 // public -> stderr, so it stays visible when stdout is piped
-process.stderr.write(JSON.stringify({ ...(await exportJWK(publicKey)), alg, use: 'sig' }) + '\n')
+process.stderr.write(JSON.stringify({ ...publicJwk, alg, use: 'sig', kid }) + '\n')
